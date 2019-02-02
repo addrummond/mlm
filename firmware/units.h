3dfaa989c98b8d0412f190d3727106b54b037fca
Alex Drummond <a.d.drummond@gmail.com>
2019-02-02 21:29:10 +0000

    lux calculation tweaks

--- firmware/units.h
+++ firmware/units.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <sensor.h>
 
-#define EV_BPS 11 // ev and lux values stored with this number of binary places
+#define EV_BPS 10 // ev and lux values stored with this number of binary places
 
 int32_t lux_to_ev(int32_t lux);
 int32_t sensor_reading_to_lux(sensor_reading r, int32_t gain, int32_t integ_time);
