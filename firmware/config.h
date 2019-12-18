17d844d6bc238a9c28eac7835c504a4073c519c3
Alex Drummond <a.d.drummond@gmail.com>
2019-12-18 19:36:31 +0000

    update batsense pin and port

--- firmware/config.h
+++ firmware/config.h
@@ -8,8 +8,8 @@
 #define REGMODE_PORT                           gpioPortB
 #define REGMODE_PIN                            13
 
-#define BATSENSE_PORT                          gpioPortB
-#define BATSENSE_PIN                           14
+#define BATSENSE_PORT                          gpioPortC
+#define BATSENSE_PIN                           15
 
 #define LONG_BUTTON_PRESS_MS                   200
 #define DOUBLE_TAP_INTERVAL_MS                 400
