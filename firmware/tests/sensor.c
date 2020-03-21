279045a5c12a568264e41b11237404e2cb8e5fc5
Alex Drummond <a.d.drummond@gmail.com>
2020-03-21 10:52:07 +0000

    improve output of sensor test

--- firmware/tests/sensor.c
+++ firmware/tests/sensor.c
@@ -21,6 +21,8 @@ int test_main()
         sensor_reading sr = sensor_get_reading_auto(delay_ms, &gain, &itime);
         int32_t lux = sensor_reading_to_lux(sr, gain, itime);
         int32_t ev = lux_to_reflective_ev(lux);
+        int ap_index, ss_index, third;
+        ev_iso_aperture_to_shutter(ev, ISO_100, F8_AP_INDEX, &ap_index, &ss_index, &third);
         int32_t evthird = ev & ((1<<EV_BPS)-1);
         int32_t thirdval = 0;
         if (evthird > (2<<EV_BPS)/3) {
@@ -28,7 +30,7 @@ int test_main()
         } else if (evthird > (1 << EV_BPS)/3) {
             thirdval = 1;
         }
-        SEGGER_RTT_printf(0, "READING g=%u itime=%u c0=%u c1=%u lux=%u/%u (%u) ev=%s%u/%u (%u+%u/3)\n", gain, itime, sr.chan0, sr.chan1, lux, 1<<EV_BPS, lux>>EV_BPS, sign_of(ev), iabs(ev), 1<<EV_BPS, ev>>EV_BPS, thirdval);
+        SEGGER_RTT_printf(0, "READING g=%u itime=%u c0=%u c1=%u lux=%u/%u (%u) ev=%s%u/%u (%u+%u/3) -- %s %s @ 100 (%u %u)\n", gain, itime, sr.chan0, sr.chan1, lux, 1<<EV_BPS, lux>>EV_BPS, sign_of(ev), iabs(ev), 1<<EV_BPS, ev>>EV_BPS, thirdval, ss_strings[ss_index], ap_strings[ap_index], ss_index, ap_index);
         //leds_all_off();
         //led_on(LED_1S_N + ss_index);
         //if (third == -1)
