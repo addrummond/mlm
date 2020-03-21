186fa29a4332544896bc12168e32a25d2cbf2bb6
Alex Drummond <a.d.drummond@gmail.com>
2020-03-21 17:46:30 +0000

    try new approach to ranging

--- firmware/sensor.c
+++ firmware/sensor.c
@@ -133,142 +133,103 @@ bool sensor_has_valid_data()
     return !(status & 0b10000000);
 }
 
-static inline int32_t subtract_margin(int32_t max_lux)
-{
-    return (int32_t)((int64_t)max_lux * 85LL / 100LL);
-}
-
-// def output_tables():
-//     def f(c0,c1,a,b,gain,itime):
-//         return (a * c0 + b * c1) / gain / itime
-//     print("static const int LUX_VAL_DOWN_SHIFT = 5;")
-//     rati = 1
-//     for rat in [(1.7743, 1.1059, 0.44), (4.2785, -1.9548, 0.45), (0.5926, 0.1185, 0.84)]:
-//         print("static const uint16_t RAT_%i_MAX_LUX_VALS[8][6] = {" % rati)
-//         for itime in reversed([50.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0]):
-//             print("    {", end='')
-//             for gain in reversed([1.0, 2.0, 4.0, 8.0, 48.0, 96.0]):
-//                 c0, c1 = 0.0, 0.0
-//                 if rat[2] < 0.5:
-//                     c0 = 65535.0
-//                     c1 = -((65535.0 * rat[2])/(rat[2] - 1.0))
-//                 else:
-//                     c1 = 65535.0
-//                     c0 = c1 * (1.0/rat[2] - 1.0)
-//                 maxlux = f(c0, c1, rat[0], rat[1], gain, itime/100.0)
-//                 print("%i," % (int(round(maxlux)) >> 5), end='')
-//             print("},")
-//         rati += 1
-//         print("};")
-static const int LUX_VAL_DOWN_SHIFT = 5;
-static const uint16_t RAT_1_MAX_LUX_VALS[8][6] = {
-    {14,28,169,338,676,1353,},
-    {16,32,193,386,773,1546,},
-    {18,37,225,451,902,1804,},
-    {22,45,270,541,1082,2165,},
-    {28,56,338,676,1353,2706,},
-    {37,75,451,902,1804,3608,},
-    {56,112,676,1353,2706,5413,},
-    {112,225,1353,2706,5413,10826,},
-};
-static const uint16_t RAT_2_MAX_LUX_VALS[8][6] = {
-    {14,28,171,342,685,1371,},
-    {16,32,195,391,783,1567,},
-    {19,38,228,457,914,1828,},
-    {22,45,274,548,1097,2194,},
-    {28,57,342,685,1371,2743,},
-    {38,76,457,914,1828,3657,},
-    {57,114,685,1371,2743,5486,},
-    {114,228,1371,2743,5486,10973,},
-};
-static const uint16_t RAT_3_MAX_LUX_VALS[8][6] = {
-    {1,2,14,29,59,118,},
-    {1,2,16,33,67,135,},
-    {1,3,19,39,78,157,},
-    {1,3,23,47,94,189,},
-    {2,4,29,59,118,236,},
-    {3,6,39,78,157,315,},
-    {4,9,59,118,236,473,},
-    {9,19,118,236,473,947,},
-};
-
 static void get_mode(sensor_reading r, int32_t *itime, int *itime_key, int32_t *gain, int *gain_key)
 {
     int32_t lux50 = sensor_reading_to_lux(r, 1, 50);
     SEGGER_RTT_printf(0, "LUX50 = %u/%u = %u (c0=%u,c1=%u)\n", lux50, (1<<EV_BPS), lux50/(1<<EV_BPS), r.chan0, r.chan1);
     int32_t rat = ((int32_t)r.chan1 * 100) / ((int32_t)r.chan0 + (int32_t)r.chan1);
 
-    const uint16_t *tab;
-    if (rat < 45)
-        tab = RAT_1_MAX_LUX_VALS;
-    else if (rat < 64)
-        tab = RAT_2_MAX_LUX_VALS;
-    else
-        tab = RAT_3_MAX_LUX_VALS;
-    
-    // use longest integration time possible.
-    // could use binary search here, but performance of linear search is fine
+    static const int16_t itimes[] = {
+        400, 350, 300, 250, 200, 150, 100, 50
+    };
+    static const int8_t gains[] = {
+        1, 2, 4, 8, 48, 96
+    };
+
+    static const int32_t NOMINAL_MAX_CHAN = 6000;
+
     int i, j;
-    for (i = 0; i < 8; ++i) {
-        for (j = 0; j < 6; ++j) {
-            uint32_t maxlux = tab[i*8+j] << LUX_VAL_DOWN_SHIFT << EV_BPS;
-            if (lux50 < maxlux / 2)
-                goto breakout;
+    if (rat >= 0.85) {
+        i = sizeof(itimes)/sizeof(itimes[0]) - 1;
+        j = 0;
+    } else {
+        // Figure out the longest possible itime we can use.
+        // Could use a binary search here, but a linear search performs well enough.
+        for (int i = 0; i < sizeof(itimes)/sizeof(itimes[0]); ++i) {
+            for (int j = 0; j < sizeof(gains)/sizeof(gains[0]); ++j) {
+                int32_t max;
+                if (rat < 45) {
+                    int32_t ch1 = (rat * NOMINAL_MAX_CHAN) / (100 - rat);
+                    max = ((17743*NOMINAL_MAX_CHAN + 11059*ch1) * 10) / gains[j] / itimes[i];
+                } else if (rat < 64) {
+                    int32_t ch0 = ((100 - rat) * NOMINAL_MAX_CHAN) / rat;
+                    max = ((42785*ch0 + 19548*NOMINAL_MAX_CHAN) * 10) / gains[j] / itimes[i];
+                } else { // if (rat < 0.85)
+                    int32_t ch0 = ((100 - rat) * NOMINAL_MAX_CHAN) / rat;
+                    max = ((42785*ch0 + 19548*NOMINAL_MAX_CHAN) * 10) / gains[j] / itimes[i];
+                }
+
+                // max is in lux*10000. Convert to 1 << EV_BPS
+                // + 1 so that we get a slight underestimate rather than a slight overestimate.
+                max /= (10000 / (1 << EV_BPS)) + 1;
+
+                if (max > (lux50*11) / 10)
+                    break;
+            }
         }
     }
 
-breakout:
+    *itime = itimes[i];
+    *gain = gains[j];
+
     switch (i) {
         case 7:
-            *itime = 50, *itime_key = ITIME_50;
+            *itime_key = ITIME_50;
             break;
         case 6:
-            *itime = 100, *itime_key = ITIME_100;
+            *itime_key = ITIME_100;
             break;
         case 5:
-            *itime = 150, *itime_key = ITIME_150;
+            *itime_key = ITIME_150;
             break;
         case 4:
-            *itime = 200, *itime_key = ITIME_200;
+            *itime_key = ITIME_200;
             break;
         case 3:
-            *itime = 250, *itime_key = ITIME_250;
+            *itime_key = ITIME_250;
             break;
         case 2:
-            *itime = 300, *itime_key = ITIME_300;
+            *itime_key = ITIME_300;
             break;
         case 1:
-            *itime = 350, *itime_key = ITIME_350;
+            *itime_key = ITIME_350;
             break;
         case 0:
-            *itime = 400, *itime_key = ITIME_400;
-            break;
-        default:
-            *itime = 50, *itime_key = ITIME_50;
+            *itime_key = ITIME_400;
             break;
     }
 
     switch (j) {
-        case 5:
-            *gain = 1, *gain_key = GAIN_1X;
-            break;
-        case 4:
-            *gain = 2, *gain_key = GAIN_2X;
-            break;
-        case 3:
-            *gain = 4, *gain_key = GAIN_4X;
-            break;
-        case 2:
-            *gain = 8, *gain_key = GAIN_8X;
+        case 0:
+            *gain_key = GAIN_1X;
             break;
         case 1:
-            *gain = 48, *gain_key = GAIN_48X;
+            *gain_key = GAIN_2X;
             break;
-        case 0:
-            *gain = 96, *gain_key = GAIN_96X;
+        case 2:
+            *gain_key = GAIN_4X;
+            break;
+        case 3:
+            *gain_key = GAIN_8X;
+            break;
+        case 4:
+            *gain_key = GAIN_48X;
+            break;
+        case 5:
+            *gain_key = GAIN_96X;
             break;
         default:
-            *gain = 1, *gain_key = GAIN_1X;
+            *gain_key = GAIN_1X;
             break;
     }
 }
