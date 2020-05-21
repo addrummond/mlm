1d4821aebf44a5c6daa187e0645eb831665212ed
Alex Drummond <a.d.drummond@gmail.com>
2020-05-21 22:58:44 +0100

    update iso logic for new iso dial labeling

--- firmware/iso.c
+++ firmware/iso.c
@@ -2,19 +2,22 @@
 
 bool iso_dial_pos_can_go_third_below(int pos)
 {
-    return pos == 0 || pos > 21;
+    return (pos >= 0 && pos <= 4) || pos >= 20;
 }
 
 bool iso_dial_pos_can_go_third_above(int pos)
 {
-    return pos > 20;
+    return (pos >= 0 && pos <= 3) || pos >= 19;
 }
 
 int32_t iso_dial_pos_and_third_to_iso(int pos, int third)
 {
-    if (pos <= 20)
-        return pos + ISO_12;
-    return ISO_1600 + 3*(pos - 21) + third;
+    if (pos >= 0 && pos <= 4)
+        return 3*pos + ISO_3 + third;
+    if (pos >= 5 && pos <= 18)
+        return pos - 5 + ISO_64;
+    if (pos >= 19)
+        return 3*(pos - 19) + ISO_1600 + third;
 }
 
 int iso_dial_pos_to_led_n(int pos)
--- firmware/iso.h
+++ firmware/iso.h
@@ -9,12 +9,14 @@
 // units, we starts from 0 = ISO 0.8 (the lowest ISO number).
 
 #define ISO_0_PT_8           0
+#define ISO_3                6
 #define ISO_12               12 // just a coincidence that ISO 12 == 12
+#define ISO_64               19
 #define ISO_100              21
 #define ISO_1600             33
 #define ISO_MIN              (ISO_12 - 1)
 #define ISO_MAX              (ISO_12 + (7*3) + (2*3) + 1)
-#define ISO_100_DIAL_POS     9
+#define ISO_100_DIAL_POS     7
 #define ISO_N_DIAL_POSITIONS 24
 
 int32_t iso_dial_pos_and_third_to_iso(int pos, int third);
