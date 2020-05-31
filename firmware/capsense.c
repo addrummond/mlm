b7462a4d6e2aca4dd20240e25e868e7497769662
Alex Drummond <a.d.drummond@gmail.com>
2020-05-31 13:16:44 +0100

    TEMP NOT BAD

--- firmware/capsense.c
+++ firmware/capsense.c
@@ -29,7 +29,7 @@ uint32_t le_calibration_center_pad_threshold __attribute__((section (".persisten
 
 static const uint32_t THRESHOLD_FRAC = 223;
 // The left and right buttons are often pressed with the edge of the thumb, so a little more sensitivity is needed.
-static const uint32_t LEFT_RIGHT_THRESHOLD_FRAC = 232;
+static const uint32_t LEFT_RIGHT_THRESHOLD_FRAC = 227;
 
 static const PCNT_Init_TypeDef initPCNT =
 {
